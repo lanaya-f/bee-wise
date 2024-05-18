1cdbfa4f38ff6c9273639b36adade2caffc03627
Dexter Orot <82924406+sumire34@users.noreply.github.com>
2024-05-18 17:40:05 +0800

    Add files via upload

---
+++ codes staging/sketch_may18a.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+const int MIC_PIN = 34;
+const int LED_PIN = 2;
+
+unsigned long startTime = 0;
+volatile unsigned long period = 0;
+volatile boolean pulseDetected = false;
+
+void IRAM_ATTR handleInterrupt() {
+  unsigned long currentTime = micros();
+  period = currentTime - startTime;
+  startTime = currentTime;
+  pulseDetected = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(MIC_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(MIC_PIN), handleInterrupt, RISING);
+}
+
+void loop() {
+  if (pulseDetected) {
+    float frequency = 1000000.0 / period;
+    Serial.print("Frequency: ");
+    Serial.print(frequency);
+    Serial.println(" Hz");
+    
+    pulseDetected = false;
+    digitalWrite(LED_PIN, HIGH);
+    delay(10);
+    digitalWrite(LED_PIN, LOW); 
+  }
+}
